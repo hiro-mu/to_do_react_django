4870445fbd2e05be60114a757beb83e88176e01c
hiro-mu <wad.akico.0926@icloud.com>
2023-12-30 21:28:51 +0900

    tagを追加

--- backend_Project/backend/todo/admin.py
+++ backend_Project/backend/todo/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from .models import Todo
+from .models import Todo, Tag
 
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'completed')
+    list_display = ('title', 'description', 'completed', '_tags')
 
-# Register your models here.
+    def _tags(self, row):
+        return ', '.join([x.name for x in row.tag.all()])
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    
+    def _str_(self):
+        return self.name
 
 admin.site.register(Todo, TodoAdmin)
+admin.site.register(Tag, TagAdmin)
--- backend_Project/backend/todo/models.py
+++ backend_Project/backend/todo/models.py
@@ -6,6 +6,13 @@ class Todo(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    tag = models.ManyToManyField("Tag", related_name="tags", blank=True)
 
     def _str_(self):
         return self.title
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def _str_(self):
+        return self.name
